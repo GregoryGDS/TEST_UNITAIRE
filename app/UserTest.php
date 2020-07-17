50efca83e904769a3ac505f7c92ce79dc601b54c
gregory <gregory.dossantos98@gmail.com>
2020-07-17 11:22:53 +0200

    setUp test

--- app/UserTest.php
+++ app/UserTest.php
@@ -9,56 +9,61 @@ class UserTest extends TestCase
 {
     private $testValid;
     private $testInvalid;
+    private $testInvalidEmail;
+    private $testInvalidPrenom;
+    private $testInvalidNom;
+    private $testInvalidAge;
 
-    public function testValid(){
+    
+    public function setUp():void{
         $this->testValid = new User('testValNom', 'testValPrenom', 'testVal@gmail.com', 30);
+        $this->testInvalid = new User('testInValNom', 'testInValPrenom', 'testInValMail', 12);
+        $this->testInvalidEmail = new User( 'invalNom', 'invalPrenom','inVal', 13);
+        $this->testInvalidPrenom = new User( 'invalNom', '','inVal@gmail.com', 13);
+        $this->testInvalidNom = new User( '', 'invalPrenom','inVal@gmail.com', 13);
+        $this->testInvalidAge = new User( 'invalNom', 'invalPrenom','inVal@gmail.com', 9);
+    }
+    
+    public function testValid(){
         $this->assertTrue($this->testValid->isValid());
     }
 
     public function testInvalid(){
-        $this->testInvalid = new User('testInValNom', 'testInValPrenom', 'testInValMail', 12);
+        
         $this->assertTrue($this->testInvalid->isValid());
     }
 
     public function testEmailValid(){
-        $this->testValid = new User('testValNom', 'testValPrenom', 'testVal@gmail.com', 30);
         $this->assertNotEmpty($this->testValid->getEmail());
     }
 
     public function testPrenomValid(){
-        $this->testValid = new User('testValNom', 'testValPrenom', 'testVal@gmail.com', 30);
         $this->assertNotEmpty($this->testValid->getPrenom());
     }
 
     public function testNomValid(){
-        $this->testValid = new User('testValNom', 'testValPrenom', 'testVal@gmail.com', 30);
         $this->assertNotEmpty($this->testValid->getNom());
     }
 
     public function testAgeValid(){
-        $this->testValid = new User('testValNom', 'testValPrenom', 'testVal@gmail.com', 30);
         $this->assertLessThan($this->testValid->getAge(), 13);
     }
 
 
 
     public function testEmailInvalid(){
-        $this->testInvalid = new User( 'invalNom', 'invalPrenom','inVal', 13);
-        $this->assertNotEmpty($this->testInvalid->getEmail());
+        $this->assertNotEmpty($this->testInvalidEmail->getEmail());
     }
 
     public function testPrenomInvalid(){
-        $this->testInvalid = new User( 'invalNom', '','inVal@gmail.com', 13);
-        $this->assertNotEmpty($this->testInvalid->getPrenom());
+        $this->assertNotEmpty($this->testInvalidPrenom->getPrenom());
     }
 
     public function testNomInvalid(){
-        $this->testInvalid = new User( '', 'invalPrenom','inVal@gmail.com', 13);
-        $this->assertNotEmpty($this->testInvalid->getNom());
+        $this->assertNotEmpty($this->testInvalidNom->getNom());
     }
 
-    public function testAgeInvalid(){
-        $this->testInvalid = new User( 'invalNom', 'invalPrenom','inVal@gmail.com', 9);
-        $this->assertLessThan($this->testInvalid->getAge(), 13);
+    public function testAgeInvalid(){   
+        $this->assertLessThan($this->testInvalidAge->getAge(), 13);
     }
 }
